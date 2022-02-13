44db2625eda9ee41a5b1e5e7e04c8258137a7fd1
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-02-13 16:28:06 -0700

    Implemented TheAssocTyExt on ZST<T>

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ use const_trait_impl::unconst_trait_impl;
 use core::{default::Default, marker::PhantomData};
 #[cfg(const_impl)]
 use remove_macro_call::remove_macro_call;
+use the_assoc_ty_ext::TheAssocTyExt;
 
 // Since ZST is both Eq and and PartialEq, it has structural match
 // https://github.com/rust-lang/rust/issues/63438
@@ -24,6 +25,13 @@ unconst_trait_impl! {
         fn default() -> Self {
             ZST(Default::default())
         }
+    }
+}
+
+#[cfg_attr(const_impl, remove_macro_call)]
+unconst_trait_impl! {
+    impl<T: ?Sized> const TheAssocTyExt for ZST<T> {
+        type TheAssocTy = T;
     }
 }
 
