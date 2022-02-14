f391f51e333caa02036f6d5682dee64d4a2e073f
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-02-13 19:03:50 -0700

    Prepared for publishing

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,37 @@ use core::{default::Default, marker::PhantomData};
 use remove_macro_call::remove_macro_call;
 pub use the_assoc_ty_ext::TheAssocTyExt;
 
+/// [1-ZST](https://github.com/rust-lang/unsafe-code-guidelines/issues/172), i.e. one-aligned
+/// zero-sized type.
+/// 
+/// # Example
+/// ```rust
+/// use zst::ZST;
+/// use core::mem::{size_of, size_of_val};
+/// 
+/// // Repr is necessary to ensure the size of the discriminant
+/// #[repr(u8)]
+/// enum PrimUnsignedIntKinds {
+///     U8(ZST<u8>),
+///     U16(ZST<u16>),
+///     U32(ZST<u32>),
+///     U64(ZST<u64>),
+///     U128(ZST<u128>),
+///     Usize(ZST<usize>),
+/// }
+/// 
+/// assert_eq!(size_of::<ZST<u16>>(), 0);
+/// assert_eq!(
+///     size_of_val(&PrimUnsignedIntKinds::U16(ZST::<u16>::default())),
+///     size_of::<u8>()
+/// );
+/// // Since the ZST<T> is 1-aligned (#[repr(align(1))]), the following is guaranteed to hold
+/// assert_eq!(
+///     size_of::<PrimUnsignedIntKinds>(),
+///     size_of::<core::mem::Discriminant<PrimUnsignedIntKinds>>()
+/// );
+/// ```
+/// 
 // Since ZST is both Eq and and PartialEq, it has structural match
 // https://github.com/rust-lang/rust/issues/63438
 #[derive(Clone, Debug, Hash, Eq, Ord, PartialEq, PartialOrd, Copy)]
